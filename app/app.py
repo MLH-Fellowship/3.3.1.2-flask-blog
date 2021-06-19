f39e5b72e76f5ee26fbeaf7317100d0978305621
Guillermo <Sanguillermo98@gmail.com>
2021-06-19 13:51:58 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -24,5 +24,5 @@ def profile(profile):
                             profile_data=profile_data)
 
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
