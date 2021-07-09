32b08258f68af215962100bfe9c1f7ca6d6f0a1d
Guillermo <Sanguillermo98@gmail.com>
2021-07-08 17:29:59 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,25 +6,30 @@ from flask_migrate import Migrate
 
 from .backgrounds import get_random_background
 from .profileInfo import get_profile_data
+
 # from app.db import get_db
 # from . import db
 
 
 app = Flask(__name__)
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB'))
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -36,55 +41,59 @@ class UserModel(db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
-@app.route('/')
+
+@app.route("/")
 def index():
     homeData = get_profile_data("data/home.json")
-    return render_template('index.html',
-                           title="MLH Fellow",
-                           url=os.getenv("URL"),
-                           random_background=get_random_background(),
-                           homeData=homeData)
+    return render_template(
+        "index.html",
+        title="MLH Fellow",
+        url=os.getenv("URL"),
+        random_background=get_random_background(),
+        homeData=homeData,
+    )
 
-@app.route('/<profile>')
+
+@app.route("/<profile>")
 def profile(profile):
     profile_data = get_profile_data("data/{profile}.json".format(profile=profile))
 
-    return render_template('profile.html', 
-                            profile_data=profile_data)
+    return render_template("profile.html", profile_data=profile_data)
 
-@app.route('/login', methods=('GET', 'POST'))
+
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
 
         if error is None:
             return "Login Successful", 200
         else:
             return error, 418
-    
+
     ## TODO: Return a login page
-    return render_template('login.html', action_type="login"), 200
+    return render_template("login.html", action_type="login"), 200
 
 
-@app.route('/register', methods=('GET', 'POST'))
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
@@ -97,13 +106,13 @@ def register():
             return error, 418
 
     ## TODO: Return a restister page
-    return render_template('login.html', action_type="register"), 200
+    return render_template("login.html", action_type="register"), 200
 
 
-
-@app.route('/health')
+@app.route("/health")
 def health():
-    return 'Working!'
+    return "Working!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
